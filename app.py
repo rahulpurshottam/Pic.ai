2016cc946f8f3f68c29377ad8058bb6a9e543aa9
rahulpurshottam <r77933079@gmail.com>
2025-04-07 18:44:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -100,5 +100,7 @@ def pred():
 
 
     
-if __name__=='__main__':
-    app.run(host='0.0.0.0' ,debug=True)
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 10000))  # default 10000 in case you're running locally
+    app.run(host='0.0.0.0', port=port, debug=True)
